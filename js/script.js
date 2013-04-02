bef5a15f6b62f519be0954f3d73daf9fda9da364
Julianne Jensen <JuliDanielle@Juliannes-MacBook-Pro.local>
2013-04-02 01:28:32 -0400

    jquery added to description divs

--- js/script.js
+++ js/script.js
@@ -15,36 +15,62 @@
    	 $("#third-page").hide();
      $(".course > div").hide()
      $("#lunch").show();
-	}); */
+	}); 
 	
+	$(".ingredients > h3").on("click",function(){
+		$(".ingredients-content").hide()
+		$(".ingredients-content > ul").show();
+	});*/
 	
 $(document).ready(function() {	
 	
 	$("#big-books").hide();
+	$("#book-descriptions").hide();
 	
 	$("#small-book_1 > img").on("click",function(){
 	$("#big-books").show();
+	$("#book-descriptions").hide();
 	$(".title > div").hide()
 	$("#big-book_1").show();
    });
    
     $("#small-book_2 > img").on("click",function(){
     $("#big-books").show();
+    $("#book-descriptions").hide();
     $(".title > div").hide()
     $("#big-book_2").show();
    });
    
    $("#small-book_3 > img").on("click",function(){
     $("#big-books").show();
+    $("#book-descriptions").hide();
     $(".title > div").hide()
     $("#big-book_3").show();
    });
    
    $("#small-book_4 > img").on("click",function(){
     $("#big-books").show();
+    $("#book-descriptions").hide();
     $(".title > div").hide()
     $("#big-book_4").show();
    });
-
+   
+   $("#big-book_1 > img").on("click",function(){
+   $("#book-descriptions").show();
+   $(".description > div").show()
+   $("#description_1").show();
+   $("#description_2").hide();
+   });
+   
+   $("#big-book_2 > img").on("click",function(){
+   $("#book-descriptions").show();
+   $(".description > div").show()
+   $("#description_1").hide();
+   $("#description_2").show();
+   
+   });
+   
  });
+ 
+ 
 	
