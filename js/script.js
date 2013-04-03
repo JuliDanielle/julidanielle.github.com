a6b46188eb524e52a473a6dd80172b007ef4e17f
Julianne Jensen <JuliDanielle@Juliannes-MacBook-Pro.local>
2013-04-03 00:03:35 -0400

    Added books covers to second year

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {	
+
+/* Start hide all but year nav */
 	
+	$("#small-books").hide();
+	$("#small-books2").hide();
 	$("#big-books").hide();
+	$("#big-books2").hide();
 	$("#book-descriptions").hide();
+
+/* Year_1 calls small books */
 	
+	$(".year_1").on("click",function(){
+	$("#small-books").show();
+	$("#book-descriptions").hide();
+	$(".title > div").hide()
+	$("#big-books2").hide();
+   });
+
+/* Small book_1,2,3... calls big book_1,2,3... */
+
 	$("#small-book_1 > img").on("click",function(){
 	$("#big-books").show();
 	$("#book-descriptions").hide();
@@ -31,7 +47,23 @@ $(document).ready(function() {
     $("#big-book_4").show();
    });
    
-   $("#big-book_1 > img").on("click",function(){
+   $("#small-book_5 > img").on("click",function(){
+    $("#big-books").show();
+    $("#book-descriptions").hide();
+    $(".title > div").hide()
+    $("#big-book_5").show();
+   });
+   
+   $("#small-book_6 > img").on("click",function(){
+    $("#big-books").show();
+    $("#book-descriptions").hide();
+    $(".title > div").hide()
+    $("#big-book_6").show();
+   });
+   
+/* Big book_1,2,3... calls description_1,2,3 */
+   
+  $("#big-book_1 > img").on("click",function(){
    $("#book-descriptions").show();
    $(".description > div").show()
    $("#description_1").show();
@@ -43,9 +75,84 @@ $(document).ready(function() {
    $(".description > div").show()
    $("#description_1").hide();
    $("#description_2").show();
+   }); 
    
+   $(".show_it1").click(function () { 
+   	$("#description_1").show("slow"); 
+   	$("#description_2").hide();
+   	$("#description_3").hide();
+   	$("#description_4").hide();
    });
    
+   $(".show_it2").on("click",function(){
+    $("#description_2").show();
+    $("#description_1").hide();
+    $("#description_3").hide();
+    $("#description_4").hide();
+   });
+    
+    $(".show_it3").on("click",function(){
+    $("#description_3").show();
+    $("#description_2").hide();
+    $("#description_4").hide();
+    $("#description_1").hide();
+    });
+    
+/* Year_2 calls small books_2 */
+  
+  $(".year_2").on("click",function(){
+	$("#small-books2").show();
+	$("#small-books").hide();
+	$("#book-descriptions").hide();
+	$(".title > div").hide()
+	$("#big-books").hide();
+   });
+   
+/* Small book2_1,2,3... calls big book2_1,2,3... */
+	
+  $("#small-book2_1 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_1").show();
+   });
+   
+   $("#small-book2_2 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_2").show();
+   });
+   
+   $("#small-book2_3 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_3").show();
+   });
+   
+   $("#small-book2_4 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_4").show();
+   });
+   
+   $("#small-book2_5 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_5").show();
+   });
+   
+   $("#small-book2_6 > img").on("click",function(){
+    $("#big-books2").show();
+    $("#book-descriptions").hide();
+    $(".title2 > div").hide()
+    $("#big-book2_6").show();
+   });
+   
+  
  });
  
  
