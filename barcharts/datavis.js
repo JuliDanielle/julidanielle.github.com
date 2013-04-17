d0eeb0b17b40588281c9e51153c0de6865353262
Julianne Jensen <JuliDanielle@Juliannes-MacBook-Pro.local>
2013-04-17 00:38:45 -0400

    uploaded barchart hwk assignment

---
+++ barcharts/datavis.js
@@ -0,0 +1,59 @@
+$(function() {
+	$("<p>").text("boston crime barchart")
+			.appendTo("body");
+
+});
+
+$(function() {
+
+  
+    var i = 0;
+    while(i < data.length) {
+      var name = data[i];
+      var fullName = name.name;
+      var inc = parseInt(name.incidents);
+      var dom = parseInt(name.domestic);
+      
+      var maxIncident = 10652;
+      var maxDom = 2841;
+      
+      var incFraction = inc / maxIncident;
+      var domFraction = dom / maxDom;
+      
+      var incWidth = incFraction * 5000;
+   	  var domWidth = domFraction * 500;
+      
+      var row = $("<div>").addClass("row");
+      
+      var neighborhoodDiv = $("<div>")
+      				.addClass("neighborhood")
+      				.text(fullName + " ")
+      				.appendTo(row);
+      
+      var incidentDiv = $("<div>")
+      					.css({ width: incWidth })
+      					.addClass("incident")
+      					.text(inc)
+      					.appendTo(row);
+       var domesticDiv = $("<div>")
+      					.css({ width: domWidth })
+      					.addClass("domestic")
+      					.text(dom)
+      					.appendTo(row);
+      
+      
+   /*   var clear = $("<div>")
+    				.addClass("clear");
+    
+    $("body").append(clear);
+    
+    */
+
+      			
+      $("p").append(row); 
+      console.log(name)
+      i++;
+    }
+    
+
+});
