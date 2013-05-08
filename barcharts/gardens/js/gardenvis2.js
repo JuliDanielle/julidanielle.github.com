8667589aed44dcf7eb6711fba9d875eb2b3fedd2
Julianne Jensen <JuliDanielle@Juliannes-MacBook-Pro.local>
2013-05-08 12:19:00 -0400

    finished data vis

--- barcharts/gardens/js/gardenvis2.js
+++ barcharts/gardens/js/gardenvis2.js
@@ -1,64 +1,37 @@
 //goole map plot for gardens
 
+$(function() {
+	//$("<p>").text("public gardens")
+		//	.appendTo("body");
+
+});
 
 //what I added
 	$(function() {
 		//console.log(gardens);
 	  
-	  var mapOptions = { //drew map first
+	  /*var mapOptions = { //first draw map
         center: new google.maps.LatLng(42.3583,-71.0603 ),
-        zoom: 12,
+        zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP // ROADMAP, SATELLITE, HYBRID, TERRAIN
       };
-      var myMap = new google.maps.Map($("#map-canvas")[0],mapOptions);
-      
-    // Add in a marker:
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.3611199, -71.1466786),
-        title: "CharlesRiverCG",
-        map: myMap
-      })
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.3383955, -71.1569408),
-        title: "ChestnutHillReservoirGarden",
-        map: myMap
-      })
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.3644362, -71.1362815),
-        title: "Christian Herter Garden",
-        map: myMap
-      })
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.345578, -71.1433738),
-        title: "CommonwealthTenantsAssociationCG",
-        map: myMap
-      })
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.3532753, -71.1377981),
-        title: "JacksonMannSchoolGarden",
-        map: myMap
-      })
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(42.3556407, -71.1364847),
-        title: "PennimanRd.Garden",
-        map: myMap
-      })
-    //
+      var myMap = new google.maps.Map($("#map-canvas")[0],mapOptions);*/
 	  
 	  var minX = Infinity, maxX = -Infinity, minY = Infinity, maxY = -Infinity;
 
 	  var i = 0;
 	  while(i < gardens.data.length) {
 	     var garden = gardens.data[i];    //this variable is 'garden' singular
-	     var coordinates = gardens[11];
-	     var x = parseFloat(garden[11][0],10);	//Turn a string into #. To parse is to turn into somehting else.
-	     var y = parseFloat(garden[11][1],10);
+	     var coordinates = garden[13]; 
+	     var x = parseFloat(coordinates[1],10);	//Turn a string into #. To parse is to turn into somehting else.
+	     var y = parseFloat(coordinates[2],10);
 	     
 	     if(x < minX) { minX = x; }
 		 if(x > maxX) { maxX = x; }
 		 
 		 if(y < minY) { minY = y; }
     	 if(y > maxY) { maxY = y; }
+		 
 		 
 		 var GardenClass =$("<div>").addClass("gardens"); //add or append garden to div
 		 $(".gardens").append(garden);
@@ -70,10 +43,11 @@
    		 var screenY =  500 - (y - minY) / diffY * 500;
 		 
 		var div =$("<div>").addClass("circles").css({
-		position:"absolute", width: 10, height:10,
-		backgroundColor:"red",
+		position:"absolute", width:8, height:8,
+		backgroundColor:"orange",
+		borderRadius:"25px",
 		left: screenX, top: screenY,
-		opacity:0.5	
+		opacity:0.7	
 	})
 	    .attr("title", coordinates ); // display title is coordinates
 	    $("body").append(div);
@@ -89,6 +63,6 @@
 
 	  i++; 
 	}
-	console.log([minX, maxX]);
+	//console.log([minX, maxX]);
 
 });
